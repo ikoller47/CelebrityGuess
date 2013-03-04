ef86a0fc59ceee9efb9c8d078faa0680ea35549a
Jeff Hochgesang <j.hochgesang@gmail.com>
2013-03-03 20:57:53 -0600

    modified driver and game

--- CelebrityGuessGame/src/Driver.java
+++ CelebrityGuessGame/src/Driver.java
@@ -1,3 +1,4 @@
+
 public class Driver {
 
 	/**
@@ -5,15 +6,16 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 
-		BinTree binTree = new BinTree();
+		//BinTree binTree = new BinTree();
 //		BNode root = new BNode("Would you like to play a celebrity guessing game?");
 //		BNode right = new BNode("Barack Obama");
-		BNode root = new BNode("Barack Obama");
-		
-		binTree.theBTRootNode = root;
+		//BNode root = new BNode("Barack Obama");
+		BinaryTree tree = new BinaryTree("Barack Obama");
+		//binTree.theBTRootNode = root;
 //		root.rightBNode = right;
 		
-		Game game = new Game(binTree);
+		//Game game = new Game(binTree);
+		Game game = new Game(tree);
 		game.play();
 
 	}
--- CelebrityGuessGame/src/Game.java
+++ CelebrityGuessGame/src/Game.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
+
 public class Game {
 	
-	BinTree gameTree;
+	BinaryTree gameTree;
+	//BinTree gameTree;
 
-	public Game(BinTree tree) {
+	/*public Game(BinTree tree) {
+		gameTree = tree;
+	}*/
+	
+	public Game(BinaryTree tree) {
 		gameTree = tree;
 	}
 	
 	public void play(){
 		
-		BNode celeb = null;
+		Node celeb = null;
+//		BNode celeb = null;
 		String newCeleb = "";
 		String newQuestion = "";
 		
@@ -18,35 +25,35 @@ public class Game {
 		
 		String input = "";
 		
-		
 		while(true){
 			
-			BNode root = gameTree.theBTRootNode;
+//			BNode root = gameTree.theBTRootNode;
 //			BNode celebQuestion = root.leftBNode;
 //			BNode celebAnswer = root.rightBNode;
-		
+			Node root = gameTree.getRoot();
+			
 			System.out.println("Would you like to play a celebrity guessing game?");
 			
 			input = scanner.nextLine();
 	
 			if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
 				while(root != null){
-					if (root.leftBNode == null && root.rightBNode == null){
+					if (root.getLeftChild() == null && root.getRightChild() == null){
 						celeb = root;
-						System.out.println("Is the celebrity you are thinking of " + celeb.value + "?");
+						System.out.println("Is the celebrity you are thinking of " + celeb.getAnswer() + "?");
 						input = scanner.nextLine();
 						if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
-							root = root.leftBNode;
+							root = root.getLeftChild();
 						} else {
-							root = root.rightBNode;
+							root = root.getRightChild();
 						}
 					} else {
-						System.out.println(root.value);
+						System.out.println(root.getQuestion());
 						input = scanner.nextLine();
 						if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
-							root = root.leftBNode;
+							root = root.getLeftChild();
 						} else {
-							root = root.rightBNode;
+							root = root.getRightChild();
 //							celebQuestion = celebQuestion.leftBNode;
 						}
 						
@@ -60,19 +67,23 @@ public class Game {
 				} else {
 					System.out.println("Who are you thinking of?");
 					newCeleb = scanner.nextLine();
-					System.out.println("Ask a yes/no question that would distinguish between " + celeb.value + " and " + newCeleb);
+					System.out.println("Ask a yes/no question that would distinguish between " + celeb.getAnswer() + " and " + newCeleb);
 					newQuestion = scanner.nextLine();
 					System.out.println("Would a answer of yes indicate " + newCeleb + "?");
 					input = scanner.nextLine();
 					if(input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")){
-						BNode newCelebNode = new BNode(newQuestion);
-						newCelebNode.leftBNode = new BNode(newCeleb);
-						gameTree.insertAB(celeb, newCelebNode, true);
+						//BNode newCelebNode = new BNode(newQuestion);
+						//newCelebNode.leftBNode = new BNode(newCeleb);
+						//gameTree.insertAB(celeb, newCelebNode, true);
+						Node yesNode = gameTree.addNode(celeb, newQuestion, newCeleb);
+						celeb.setLeftChild(yesNode);
 						System.out.println("Thank you for adding " + newCeleb + " to the database.");
 					} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")) {
-						BNode newCelebNode = new BNode(newQuestion);
-						newCelebNode.rightBNode = new BNode(newCeleb);
-						gameTree.insertAB(celeb, newCelebNode, false);
+						//BNode newCelebNode = new BNode(newQuestion);
+						//newCelebNode.rightBNode = new BNode(newCeleb);
+						//gameTree.addNode(celeb, newQuestion, newCeleb);
+						Node noNode = gameTree.addNode(celeb, newQuestion, newCeleb);
+						celeb.setRightChild(noNode);
 						System.out.println("Thank you for adding " + newCeleb + " to the database.");
 					}
 				}
