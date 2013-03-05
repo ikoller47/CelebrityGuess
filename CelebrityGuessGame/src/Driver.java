bfa44d7c01c4efb70a1b3f19291d95e7d520756d
Ian Koller <ikoller47@gmail.com>
2013-03-04 21:14:12 -0600

    Integrated threaded server into game logic.

--- CelebrityGuessGame/src/Driver.java
+++ CelebrityGuessGame/src/Driver.java
@@ -1,16 +1,19 @@
+import java.net.*;
 
 public class Driver {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
+    
+    public static void main(String argv[]) throws Exception
+    {
 		BinaryTree tree = new BinaryTree("Barack Obama");
-
-		Game game = new Game(tree);
-		game.play();
-
-	}
-
+		
+        ServerSocket welcomeSocket = new ServerSocket(6001);
+        System.out.println("Server is up...");
+        while(true) {
+            Socket connectionSocket = null;
+            connectionSocket = welcomeSocket.accept();
+            Game game = new Game(connectionSocket, tree);
+            Thread t = new Thread(game);
+            t.start();
+        }
+    }
 }
--- CelebrityGuessGame/src/Game.java
+++ CelebrityGuessGame/src/Game.java
@@ -1,86 +1,106 @@
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
 
-
-public class Game {
-	
-	BinaryTree gameTree;
-	
-	public Game(BinaryTree tree) {
-		gameTree = tree;
-	}
-	
-	public void play(){
-		
-		Node celeb = null;
+public class Game implements Runnable {
+    
+	private BufferedReader inFromClient;
+    private BufferedWriter outToClient;
+    private Socket s;
+    BinaryTree gameTree;
+    
+    public Game(Socket s, BinaryTree tree) throws IOException {
+        this.s = s;
+        inFromClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        outToClient = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+        gameTree = tree;
+    }
+    
+    public void run() {
+    	
+    	Node celeb = null;
 		String newCeleb = "";
 		String newQuestion = "";
 		
-		Scanner scanner = new Scanner(System.in);
 		
-		String input = "";
-		
-		while(true){
-			
-			Node root = gameTree.root;
-			
-			System.out.println("Would you like to play a celebrity guessing game?");
-			
-			input = scanner.nextLine();
-	
-			if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
-				while(root != null){
-					if (root.getLeftChild() == null && root.getRightChild() == null){
-						celeb = root;
-						System.out.println("Is the celebrity you are thinking of " + celeb.getValue() + "?");
-						input = scanner.nextLine();
-						if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
-							root = root.getLeftChild();
-						} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")) {
-							root = root.getRightChild();
-						}
-					} else {
-						System.out.println(root.getValue());
-						input = scanner.nextLine();
-						if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
-							root = root.getLeftChild();
-						} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")) {
-							root = root.getRightChild();
-						}
-						
-					}
-					
-				}
-				
-				if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")){
-					System.out.println("I'm so smart!");
-				} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")){
-					System.out.println("Who are you thinking of?");
-					newCeleb = scanner.nextLine();
-					System.out.println("Ask a yes/no question that would distinguish between " + celeb.getValue() + " and " + newCeleb);
-					newQuestion = scanner.nextLine();
-					System.out.println("Would a answer of yes indicate " + newCeleb + "?");
-					input = scanner.nextLine();
-					if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")){
-						Node newCelebNode = new Node(newQuestion);
-						
-						gameTree.insertNode(celeb, newCelebNode, false);
-						newCelebNode.setLeftChild(new Node(newCelebNode, null, null, newCeleb));
-						
-						System.out.println("Thank you for adding " + newCeleb + " to the database.");
-					} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")) {
-						Node newCelebNode = new Node(newQuestion);
-						
-						gameTree.insertNode(celeb, newCelebNode, true); //might have been missing last night with other code
-						newCelebNode.setRightChild(new Node(newCelebNode, null, null, newCeleb));
-						
-						System.out.println("Thank you for adding " + newCeleb + " to the database.");
-					}
-				}
-			} else {
-				System.exit(0);
-			}
-		}
-		
-	}
-
+        try {
+        	
+        	while(true){
+    			
+    			Node root = gameTree.root;
+    			
+    			outToClient.write("Would you like to play a celebrity guessing game?\n");
+    			outToClient.flush();
+    			String clientMessage = inFromClient.readLine();
+                
+    			if (clientMessage.equalsIgnoreCase("Y") || clientMessage.equalsIgnoreCase("YES")) {
+    				while(root != null){
+    					if (root.getLeftChild() == null && root.getRightChild() == null){
+    						celeb = root;
+    						outToClient.write("Is the celebrity you are thinking of " + celeb.getValue() + "?\n");
+    						outToClient.flush();
+    						clientMessage = inFromClient.readLine();
+    						if (clientMessage.equalsIgnoreCase("Y") || clientMessage.equalsIgnoreCase("YES")) {
+    							root = root.getLeftChild();
+    						} else if (clientMessage.equalsIgnoreCase("N") || clientMessage.equalsIgnoreCase("NO")) {
+    							root = root.getRightChild();
+    						}
+    					} else {
+    						outToClient.write(root.getValue() + "\n");
+    						outToClient.flush();
+    						clientMessage = inFromClient.readLine();
+    						if (clientMessage.equalsIgnoreCase("Y") || clientMessage.equalsIgnoreCase("YES")) {
+    							root = root.getLeftChild();
+    						} else if (clientMessage.equalsIgnoreCase("N") || clientMessage.equalsIgnoreCase("NO")) {
+    							root = root.getRightChild();
+    						}
+    						
+    					}
+    					
+    				}
+    				
+    				if (clientMessage.equalsIgnoreCase("Y") || clientMessage.equalsIgnoreCase("YES")){
+    					outToClient.write("I'm so smart!\n");
+    					outToClient.flush();
+    				} else if (clientMessage.equalsIgnoreCase("N") || clientMessage.equalsIgnoreCase("NO")){
+    					outToClient.write("Who are you thinking of?\n");
+    					outToClient.flush();
+    					newCeleb = inFromClient.readLine();
+    					outToClient.write("Ask a yes/no question that would distinguish between " + celeb.getValue() + " and " + newCeleb + "\n");
+    					outToClient.flush();
+    					newQuestion = inFromClient.readLine();
+    					outToClient.write("Would a answer of yes indicate " + newCeleb + "?\n");
+    					outToClient.flush();
+    					clientMessage = inFromClient.readLine();
+    					if (clientMessage.equalsIgnoreCase("Y") || clientMessage.equalsIgnoreCase("YES")){
+    						Node newCelebNode = new Node(newQuestion);
+    						
+    						gameTree.insertNode(celeb, newCelebNode, false);
+    						newCelebNode.setLeftChild(new Node(newCelebNode, null, null, newCeleb));
+    						
+    						outToClient.write("Thank you for adding " + newCeleb + " to the database.\n");
+    						outToClient.flush();
+    					} else if (clientMessage.equalsIgnoreCase("N") || clientMessage.equalsIgnoreCase("NO")) {
+    						Node newCelebNode = new Node(newQuestion);
+    						
+    						gameTree.insertNode(celeb, newCelebNode, true); //might have been missing last night with other code
+    						newCelebNode.setRightChild(new Node(newCelebNode, null, null, newCeleb));
+    						
+    						outToClient.write("Thank you for adding " + newCeleb + " to the database.\n");
+    						outToClient.flush();
+    					}
+    				}
+    			} else {
+    				s.close();
+    			}
+    		}
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                s.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
