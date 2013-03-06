4616dcf60e0a23330c25d3999fe1fdd6589cb0be
Thomas Dambra <tdambra03@aol.com>
2013-03-05 21:38:09 -0600

    other refacing

--- CelebrityGuessGame/src/Game.java
+++ CelebrityGuessGame/src/Game.java
@@ -14,16 +14,20 @@ public class Game {
 		Node celeb = null;
 		String newCeleb = "";
 		String newQuestion = "";
+		String playerName = "";
 		
 		Scanner scanner = new Scanner(System.in);
 		
 		String input = "";
 		
+		System.out.println("What is your name?");
+		playerName = scanner.nextLine();
+
 		while(true){
 			
 			Node root = gameTree.root;
 			
-			System.out.println("Would you like to play a celebrity guessing game?");
+			System.out.println("\nHello " + playerName + ", Would you like to play a celebrity guessing game?");
 			
 			input = scanner.nextLine();
 	
@@ -31,7 +35,7 @@ public class Game {
 				while(root != null){
 					if (root.getLeftChild() == null && root.getRightChild() == null){
 						celeb = root;
-						System.out.println("Is the celebrity you are thinking of " + celeb.getValue() + "?");
+						System.out.println("\nIs the celebrity you are thinking of " + celeb.getValue() + "?");
 						input = scanner.nextLine();
 						if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
 							root = root.getLeftChild();
@@ -39,7 +43,7 @@ public class Game {
 							root = root.getRightChild();
 						}
 					} else {
-						System.out.println(root.getValue());
+						System.out.println("\n" + root.getValue());
 						input = scanner.nextLine();
 						if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")) {
 							root = root.getLeftChild();
@@ -52,13 +56,13 @@ public class Game {
 				}
 				
 				if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")){
-					System.out.println("I'm so smart!");
+					System.out.println("\nI'm so smart!");
 				} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")){
-					System.out.println("Who are you thinking of?");
+					System.out.println("\nWho are you thinking of?");
 					newCeleb = scanner.nextLine();
-					System.out.println("Ask a yes/no question that would distinguish between " + celeb.getValue() + " and " + newCeleb);
+					System.out.println("\nAsk a yes/no question that would distinguish between " + celeb.getValue() + " and " + newCeleb);
 					newQuestion = scanner.nextLine();
-					System.out.println("Would a answer of yes indicate " + newCeleb + "?");
+					System.out.println("\nIf someone answers yes to this question, would the answer be " + newCeleb + "?");
 					input = scanner.nextLine();
 					if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("YES")){
 						Node newCelebNode = new Node(newQuestion);
@@ -66,17 +70,18 @@ public class Game {
 						gameTree.insertNode(celeb, newCelebNode, false);
 						newCelebNode.setLeftChild(new Node(newCelebNode, null, null, newCeleb));
 						
-						System.out.println("Thank you for adding " + newCeleb + " to the database.");
+						System.out.println("\nThank you for adding " + newCeleb + " to the database.");
 					} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("NO")) {
 						Node newCelebNode = new Node(newQuestion);
 						
 						gameTree.insertNode(celeb, newCelebNode, true); //might have been missing last night with other code
 						newCelebNode.setRightChild(new Node(newCelebNode, null, null, newCeleb));
 						
-						System.out.println("Thank you for adding " + newCeleb + " to the database.");
+						System.out.println("\nThank you for adding " + newCeleb + " to the database.");
 					}
 				}
 			} else {
+				System.out.println("\nGoodbye " + playerName + "!");
 				System.exit(0);
 			}
 		}
